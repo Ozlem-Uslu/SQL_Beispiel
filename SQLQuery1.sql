6fe23bf66cd11a93b6a2a440d1c114113eeca73b
Özlem Uslu <147069176+Ozlem-Uslu@users.noreply.github.com>
2023-12-22 22:14:39 +0100

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,30 +1,29 @@
-﻿--2. Customers tablosundan adı‘A’ harfi ile başlayan kişileri çeken sorguyu yazınız.
+﻿--1. Schreiben Sie eine Abfrage, die Personen aus der Tabelle „Customers“ abruft, deren Namen mit dem Buchstaben „A“ beginnen.
 SELECT NAMESURNAME FROM CUSTOMERS
 WHERE NAMESURNAME LIKE 'A%'
 
---3. 1990 ve 1995 yılları arasında doğan müşterileri çekiniz. 1990 ve1995 yılları dahildir.
+--2. Schreiben Sie die Abfrage, die Kunden erfasst, die zwischen 1990 und 1995 geboren sind. Die Jahre 1990 und 1995 sind enthalten.
 SELECT ID, NAMESURNAME, BIRTHDATE FROM CUSTOMERS
 WHERE YEAR(BIRTHDATE) BETWEEN 1990 AND 1995
 
---4. İstanbul’da yaşayan kişileri Join kullanarak getiren sorguyu yazınız.
+--3. Schreiben Sie die Abfrage, die Menschen, die in Istanbul leben, über Join anlockt.
 SELECT C.ID, C.NAMESURNAME, CT.CITY
 FROM CUSTOMERS C
 JOIN CITIES CT ON C.CITYID=CT.ID
 WHERE CT.CITY='İSTANBUL'
 .
---5. İstanbul’da yaşayan kişileri subquery kullanarak getiren sorguyu yazınız.
+--4. Schreiben Sie mithilfe der Unterabfrage die Abfrage, die in Istanbul lebende Personen zurückgibt.
 SELECT NAMESURNAME FROM CUSTOMERS C
 WHERE C.CITYID in (SELECT ID from CITIES where CITY='İSTANBUL')
 
---6. Hangi şehirde kaç müşterimizin olduğu bilgisini getiren sorguyu yazınız.
+--5. Schreiben Sie die Abfrage, die Auskunft darüber gibt, wie viele Kunden wir in welcher Stadt haben.
 SELECT CT.CITY, COUNT(C.ID) musteri_sayisi 
 FROM CUSTOMERS C
 JOIN CITIES CT ON CT.ID=C.CITYID
 GROUP BY CT.CITY
 ORDER BY COUNT(C.ID) DESC
 
---7. 10’dan fazla müşterimiz olan şehirleri müşteri sayısı ile birlikte müşteri 
---sayısına göre fazladan aza doğru sıralı şekilde getiriniz. 
+--6. Listen Sie die Städte auf, in denen wir mehr als 10 Kunden haben, in der Reihenfolge von den meisten zu den wenigsten, entsprechend der Anzahl der Kunden.
 SELECT C.CITYID, CT.CITY, COUNT(C.ID)musteri_sayisi 
 FROM CUSTOMERS C
 Join CITIES CT on CT.ID=C.CITYID
@@ -32,7 +31,7 @@ GROUP BY C.CITYID, CITY
 HAVING COUNT(C.ID)> 10 
 ORDER BY 3 DESC
 
---8. Hangi şehirde kaç erkek, kaç kadın müşterimizin olduğu bilgisini getiren sorguyu yazınız.
+--7. Schreiben Sie die Abfrage, die Aufschluss darüber gibt, wie viele männliche und weibliche Kunden wir in welcher Stadt haben.
 SELECT CT.CITY, GENDER, COUNT(C.ID)  
 FROM CUSTOMERS C
 Join CITIES CT on CT.ID=C.CITYID
@@ -40,17 +39,13 @@ GROUP BY GENDER, CT.CITY
 ORDER BY CITY, GENDER
 
 
---9. Customers tablosuna yaş grubu için yeni bir alan ekleyiniz. Bu işlemi hem management 
---studio ile hem de sql kodu ile yapınız. Alanı adı AGEGROUP veritipi Varchar(50)
+--8. Fügen Sie der Tabelle „Customers“ ein neues Feld für die Altersgruppe hinzu. Tun Sie dies sowohl mit Management 
+Studio als auch mit SQL-Code. Feldname AGEGROUP-Datentyp Varchar(50)
+	
+ALTER TABLE CUSTOMERS			
+ADD AGEGROUP Varchar(50);		
 
-ALTER TABLE CUSTOMERS			--SQL KODU (ALTER TABLE) ile ALTER TABLE deyimi mevcut bir tabloya sütun eklemek, silmek veya değiştirmek için kullanılır.
-ADD AGEGROUP Varchar(50);		--ALTER TABLE ifadesi aynı zamanda mevcut bir tabloya çeşitli kısıtlamalar eklemek ve bırakmak için de kullanılır.
-
-
-
-
---10. Customers tablosuna eklediğiniz AGEGROUP alanını 20-35 yaş arası,36-45 yaş arası,
---46-55 yaş arası,55-65 yaş arası ve 65 yaş üstü olarak güncelleyiniz.
+--9. Aktualisieren Sie das Feld AGEGROUP, das Sie der Customerstabelle hinzugefügt haben, als 20–35 Jahre, 36–45 Jahre, 46–55 Jahre, 55–65 Jahre und über 65 Jahre.
 
 SELECT * FROM CUSTOMERS
 UPDATE CUSTOMERS SET AGEGROUP = '20-35'
@@ -64,49 +59,27 @@ WHERE DATEDIFF(YEAR,BIRTHDATE,GETDATE()) BETWEEN 55 AND 65
 UPDATE CUSTOMERS SET AGEGROUP = '65_yas_ustu'
 WHERE DATEDIFF(YEAR,BIRTHDATE,GETDATE()) >65
 
--- yaslari gruplandirabilmek icin yas sütunu da olusturabilirim.
-UPDATE CUSTOMERS
-SET AGE =DATEDIFF(YEAR, BIRTHDATE, GETDATE())
-SELECT * FROM CUSTOMERS
-
---11. İstanbul’da yaşayıp ilçesi ‘Kadıköy’ dışında olanları listeleyiniz.
+--10. Listen Sie diejenigen auf, die in Istanbul leben und deren Bezirk außerhalb von „Kadıköy“ liegt.
+	
 SELECT C.ID, NAMESURNAME, DISTRICT 
 FROM CUSTOMERS AS C
 JOIN CITIES AS CT ON CT.ID= C.CITYID
 JOIN DISTRICTS AS D ON D.ID=C.DISTRICTID
 WHERE CITY='İSTANBUL'and DISTRICT <> 'KADIKÖY'
 
---2.yol
-SELECT * FROM CUSTOMERS 
-WHERE CITYID IN (SELECT ID FROM CITIES WHERE CITY='İSTANBUL') 
-AND DISTRICTID NOT IN (SELECT ID FROM DISTRICTS WHERE DISTRICT='KADIKÖY')
-
---asagidaki kod ile istanbulda oturanlarin listesi 47 kisi gelir.
-SELECT NAMESURNAME, CITY 
-FROM CUSTOMERS C
-JOIN CITIES CT on C.CITYID=CT.ID
-WHERE CITY ='İSTANBUL'
-
---NOT: neden istanbulda oturup kadiköy disindakiler listesi istanbulda oturanlar ile ayni
---satir geliyor?
-
-
---12. Müşterilerimizin telefon numalarının operatör bilgisini getirmek istiyoruz. 
---TELNR1 ve TELNR2 alanlarının yanına operatör numarasını (532),(505) gibi getirmek istiyoruz. 
---Bu sorgu için gereken SQL cümlesini yazınız.
+	
+--11. Wir möchten dem Betreiber die Telefonnummern unserer Kunden mitteilen. Wir möchten die Betreibernummer wie (532), (505) neben die Felder TELNR1 und TELNR2 setzen. 
+Schreiben Sie die für diese Abfrage erforderliche SQL-Anweisung.
 
 SELECT*,
 LEFT(TELNR1,5) AS OPERATOR1,
 LEFT(TELNR2,5) AS OPERATOR2
 FROM CUSTOMERS
 
-
---13 ANLAMADIM!!
---13. Müşterilerimizin telefon numaralarının operatör bilgisini getirmek istiyoruz. 
---Örneğin telefon numaraları “50” ya da “55” ile başlayan “X” operatörü “54” 
---ile başlayan “Y” operatörü “53” ile başlayan “Z” operatörü olsun. 
---Burada hangi operatörden ne kadar müşterimiz olduğu bilgisini getirecek sorguyu yazınız.
-
+--12. Wir möchten dem Betreiber die Telefonnummern unserer Kunden mitteilen
+Angenommen, der Operator „X“, dessen Telefonnummern mit „50“ oder „55“ beginnen, ist der Operator „Y“, der mit „54“ beginnt, und der Operator „Z“ beginnt mit „53“.
+Schreiben Sie hier die Abfrage, die Aufschluss darüber gibt, wie viele Kunden wir von welchem ​​Betreiber haben.
+	
 SELECT ID, NAMESURNAME,TELNR1, TELNR2, Op1, Op2 FROM CUSTOMERS 
 UPDATE CUSTOMERS SET Op1 = 'X'
 WHERE TELNR1 like '(50%' or TELNR1 like '(55%' 
@@ -161,12 +134,8 @@ END AS TELNR2_YOPERATOR,
 END AS TELNR2_ZOPERATOR
 FROM CUSTOMERS)T
 
---Hangi operatörden nekadar müsteri kullaniyor sorgusu nasil gelir?
-
-
-
---14. Her ilde en çok müşteriye sahip olduğumuz ilçeleri müşteri sayısına göre 
---çoktan aza doğru sıralı şekilde şekildeki gibi getirmek için gereken sorguyu yazınız. 
+--13. Schreiben Sie die erforderliche Abfrage, um die Bezirke, in denen wir in jeder Provinz die meisten Kunden haben, 
+--in der Reihenfolge vom höchsten zum niedrigsten Wert entsprechend der Anzahl der Kunden anzuzeigen, wie in der Abbildung dargestellt.
 
 SELECT CITY, DISTRICT,Count(C.ID) MÜSTERI_SAYISI  
 FROM CUSTOMERS C
@@ -176,12 +145,8 @@ GROUP BY CITY, DISTRICT
 ORDER BY Count(C.ID) DESC
 
 
---15. Müşterilerin doğum günlerini haftanın günü(Pazartesi, Salı, Çarşamba..) 
---olarak getiren sorguyu yazınız.
-
-SELECT DATEPART(WEEKDAY, C.BIRTHDATE) DAY_ FROM CUSTOMERS C
---dogum günü tarihinin haftanin kacinci günü oldugu bilgisini verir.mesela 12.05 haftanin 2. günü yani dienstag imis.
-
+--14. Schreiben Sie eine Abfrage, die die Geburtstage der Kunden als Wochentag (Montag, Dienstag, Mittwoch usw.) zurückgibt.
+	
 SELECT C.ID, C.NAMESURNAME, C.BIRTHDATE, 
 DATENAME(WEEKDAY, C.BIRTHDATE) DAYNAME_ 
 FROM CUSTOMERS C
